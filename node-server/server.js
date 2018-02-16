e91eeac7ec289d89a9f4daab1de95101e49fd242
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2018-02-16 01:25:43 -0500

    hbs middleware

--- node-server/server.js
+++ node-server/server.js
@@ -1,16 +1,55 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+//middleware
+app.use(function(req, res, next){
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', function(err){
+    if(err){
+      console.log('Unable to write the server log');
+    }
+  });
+  //if no next, the other part won't be fired
+  next();
+});
+
+app.use(function(req, res, next){
+  res.render('maintenance.hbs');
+  //no next to block the following parts
+});
+
+//locate the dir place, static middleware
+app.use(express.static(__dirname + '/public'));
+
+//hbs partial
+hbs.registerHelper('getCurrentYear', function(){
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text){
+  return text.toUpperCase();
+})
+
 app.get('/', function(req, res){
-  res.send('Hello from server');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome',
+  });
 });
 
 app.get('/about', function(req, res){
-  res.send({
-    name: 'Sandy',
-    age: 24
-  })
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
 });
 
 app.get('/bad', function(req, res){
@@ -20,4 +59,6 @@ app.get('/bad', function(req, res){
   })
 });
 
-app.listen('3000');
+app.listen('3000', function(){
+  console.log('server is up at 3000');
+});
