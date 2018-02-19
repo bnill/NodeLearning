f08e55ac59b20239154d43ad6d05ddd79afbafcd
Yuyang Zhang <zhangyy@YuyangmatoMacBook-Pro.local>
2018-02-19 01:23:45 -0500

    test for async and sync functions

--- node-test/utils/utils.js
+++ node-test/utils/utils.js
@@ -3,6 +3,25 @@ module.exports.add = function(a, b){
   return a + b;
 }
 
+module.exports.asyncAdd = function(a, b, callback){
+  setTimeout(function(){
+    callback(a + b);
+  }, 1000);
+}
+
 module.exports.square = function(x){
   return x * x;
 }
+
+module.exports.asyncSquare = function(x, callback){
+  setTimeout(function(){
+    callback(x * x);
+  }, 1000);
+}
+
+module.exports.setName = function(user, fullName){
+  var names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,15 +1,49 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', function(){
   var res = utils.add(3, 5);
-  if(res !== 8){
-    throw new Error('Expected 8, but got' + res);
-  }
+  expect(res).toBe(8).toBeA('number');
 });
 
 it('should square the number', function(){
   var res = utils.square(3);
-  if(res !== 9){
-    throw new Error('Expected 9, but got ' + res);
-  }
-})
+  expect(res).toBe(9).toBeA('number');
+});
+
+it('should expect some value', function(){
+  //expect({name: 'bill'}).toEqual({name: 'bill'});
+  //expect([2, 3, 4]).toInclude(2); //toExclude
+  expect({
+    name: 'bnill',
+    age: 24
+  }).toInclude({
+    age: 24
+  });
+});
+
+//verify first and last names are set
+//assert it includes firstName and lastName with proper values
+it('assert it includes firstName and lastName with proper values', function(){
+  var user = {};
+  var res = utils.setName(user, 'Yuyang Zhang');
+
+  expect(res).toInclude({
+    firstName: 'Yuyang',
+    lastName: 'Zhang'
+  })
+});
+
+it('should test async add', function(done){
+  utils.asyncAdd(4, 3, function(sum){
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
+it('should test async square', function(done){
+  utils.asyncSquare(4, function(square){
+    expect(square).toBe(16).toBeA('number');
+    done();
+  });
+});
